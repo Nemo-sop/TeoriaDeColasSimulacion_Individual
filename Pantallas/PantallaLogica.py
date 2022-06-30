5f3a331838f7b3cc2e7e03009a1b614c6c5cc0bb
NemoBitBot <84641717+NemoBitBot@users.noreply.github.com>
2022-06-29 23:42:27 -0300

    Avanzamos bastante, queda terminar bien el df, y resolver todos los errores q seguro hay

--- Pantallas/PantallaLogica.py
+++ Pantallas/PantallaLogica.py
@@ -1,5 +1,7 @@
 from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
+
+from Auxiliar import simular
 
 
 class Pantalla(QMainWindow):
@@ -11,4 +13,43 @@ class Pantalla(QMainWindow):
         """Cargar la GUI"""
         uic.loadUi("Pantallas/Pantalla.ui", self)
 
-        #self.btn_simular.clicked.connect()
+        self.btn_simular.clicked.connect(self.metodoAuxiliar)
+
+    def metodoAuxiliar(self):
+
+        if self.duracion.text() != "":
+            simular(self, int(self.inicio.text()), int(self.duracion.text()), int(self.aII.text()), int(self.bII.text()), int(self.aLI.text()),
+                    int(self.bLI.text()), int(self.aFI.text()), int(self.bFI.text()), int(self.aLPI.text()), int(self.bLPI.text()), int(self.aLRI.text()),
+                    int(self.bLRI.text()), int(self.aFR.text()), int(self.bFR.text()), int(self.aIR.text()), int(self.bIR.text()), int(self.aLR.text()),
+                    int(self.bLR.text()), int(self.aLPR.text()), int(self.bLPR.text()), int(self.aIA.text()), int(self.bIA.text()))
+
+    def mostrarResultados(self, tabla, RK):
+        print(tabla)
+
+        fila = 0
+        self.tablaSimulacion.setRowCount(len(tabla))
+        for i in range(len(tabla)):
+            self.tablaSimulacion.setItem(fila, 0 , QTableWidgetItem(str(tabla.at[i, "Reloj"])))
+            self.tablaSimulacion.setItem(fila, 1 , QTableWidgetItem(str(tabla.at[i, "Tipo De Evento"])))
+            self.tablaSimulacion.setItem(fila, 2 , QTableWidgetItem(str(tabla.at[i, "Nombre"])))
+            self.tablaSimulacion.setItem(fila, 3 , QTableWidgetItem(str(tabla.at[i, "Tiempo Hasta La Proxima Llegada para informes"])))
+            self.tablaSimulacion.setItem(fila, 4 , QTableWidgetItem(str(tabla.at[i, "Proxima Llegada informes"])))
+            self.tablaSimulacion.setItem(fila, 5 , QTableWidgetItem(str(tabla.at[i, "Tiempo Hasta La Proxima Llegada para reservas"])))
+            self.tablaSimulacion.setItem(fila, 6 , QTableWidgetItem(str(tabla.at[i, "Proxima Llegada reservas"])))
+            self.tablaSimulacion.setItem(fila, 7 , QTableWidgetItem(str(tabla.at[i, "Proximo Ataque"])))
+            self.tablaSimulacion.setItem(fila, 8 , QTableWidgetItem(str(tabla.at[i, "Duracion del Ataque"])))
+            self.tablaSimulacion.setItem(fila, 9 , QTableWidgetItem(str(tabla.at[i, "Estado Informes"])))
+            self.tablaSimulacion.setItem(fila, 10, QTableWidgetItem(str(tabla.at[i, "Informes ocupado por"])))
+            self.tablaSimulacion.setItem(fila, 11, QTableWidgetItem(str(tabla.at[i, "Cola Informes"])))
+            self.tablaSimulacion.setItem(fila, 12, QTableWidgetItem(str(tabla.at[i, "Estado Reservas"])))
+            self.tablaSimulacion.setItem(fila, 13, QTableWidgetItem(str(tabla.at[i, "Reservas ocupado por"])))
+            self.tablaSimulacion.setItem(fila, 14, QTableWidgetItem(str(tabla.at[i, "Cola Reservas"])))
+            self.tablaSimulacion.setItem(fila, 15, QTableWidgetItem(str(tabla.at[i, "Tiempo oscioso informes"])))
+            self.tablaSimulacion.setItem(fila, 16, QTableWidgetItem(str(tabla.at[i, "Tiempo oscioso reservas"])))
+            self.tablaSimulacion.setItem(fila, 17, QTableWidgetItem(str(tabla.at[i, "Porcentaje de trabajo de alarmas"])))
+            self.tablaSimulacion.setItem(fila, 18, QTableWidgetItem(str(tabla.at[i, "Atendidos en informes"])))
+            self.tablaSimulacion.setItem(fila, 19, QTableWidgetItem(str(tabla.at[i, "Atendidos en reservas"])))
+            self.tablaSimulacion.setItem(fila, 20, QTableWidgetItem(str(tabla.at[i, "Cantidad de personas esperando"])))
+            self.tablaSimulacion.setItem(fila, 21, QTableWidgetItem(str(tabla.at[i, "Cantidad de personas atendidas"])))
+
+            fila += 1
